81026068e9fef1bfdc66accdc8c93082d8eef2ef
JavedAhmadDeveloper <106674476+JavedAhmadDeveloper@users.noreply.github.com>
2022-06-08 05:21:23 -0700

    another example of closure

--- src/Closure.js
+++ src/Closure.js
@@ -1,10 +1,28 @@
 function makeFunc() {
   var name = "Mozilla";
   function displayName() {
-    alert(name);
+    console.log(name);
   }
   return displayName;
 }
 
 var myFunc = makeFunc();
 myFunc();
+
+// another example of closure
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+var add = add5(2); // 5+2
+console.log(add); //7
+var add2 = add10(10); // 10+10
+console.log(add2); //20
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
